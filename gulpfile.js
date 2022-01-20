70dd00a0e41792f85232f28feba7757b009544ea
DESKTOP-7TS4NVF\kaans <senox57400@gmail.com>
2022-01-20 19:53:35 +0300

    gulp.js init

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const minifyImg = require('gulp-imagemin');
+const minifyJS = require('gulp-uglify');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const runSequence = require('run-sequence');
+
+gulp.task("test", () => {
+  console.log("test");
+});
